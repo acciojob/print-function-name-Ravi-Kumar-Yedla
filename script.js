80936b9f8298f16080bd5b847880448481303ad6
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-01-30 09:50:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 //your JS code here. If require
-function functionName() {
-  alert("Function name: functionName");
-}
-functionName();
+// function functionName() {
+//   alert("Function name: functionName");
+// }
+// functionName();
+ function functionName(){
+ alert("functionName")
+ 
+ }
+	 function();
+ 
